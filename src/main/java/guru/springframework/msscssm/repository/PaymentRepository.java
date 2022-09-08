eae9e5698c46ad08ecdd1a6740161e20078604b1
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-08 22:25:25 +0200

    Setup project with domain, enums for state and events , repository

---
+++ src/main/java/guru/springframework/msscssm/repository/PaymentRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscssm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import guru.springframework.msscssm.domain.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
