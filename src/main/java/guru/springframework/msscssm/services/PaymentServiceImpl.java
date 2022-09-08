06c7667d7efa5664a4ba2cf18e6039bd59fe9892
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-09-08 23:48:17 +0200

    create mathod to set statemachine to state that a payment was in

--- src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -1,7 +1,9 @@
 package guru.springframework.msscssm.services;
 
+import javax.swing.SwingWorker;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 import guru.springframework.msscssm.domain.Payment;
 import guru.springframework.msscssm.domain.PaymentEvent;
@@ -24,16 +26,47 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
+    }
+
+
+    // get payment
+    // get a new inbstance of statemachine (give it id of payment
+    // stop the statmachine
+    // set state of statemachine to the state of the payment from the DB
+    // restart statemachine
+    // return statemachine
+
+    private StateMachine<PaymentState,PaymentEvent> build(Long paymentId){
+        Payment payment = paymentRepository.getOne(paymentId);
+
+        StateMachine<PaymentState, PaymentEvent> sm =
+                stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        sm.stop();
+
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(),null,null,null));
+                });
+        sm.start();
+
+        return sm;
     }
 }
